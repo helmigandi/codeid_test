f11c322338d68b0c38187aabde93e7a7ee7eb16d
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-10 13:21:22 +0700

    add: error handler

---
+++ services/users/middlewares/errorHandler.js
@@ -0,0 +1,23 @@
+const errorHandler = (err, req, res, next) => { 
+  const { message } = err
+  switch (message) {
+    case "DataNotFound":
+      res.status(404).json({
+        message: 'Sorry. Data not found.'
+      })
+      break;
+    case "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters":
+      res.status(404).json({
+        message: "Sorry. The user id is invalid."
+      })
+      break;
+    default:
+      console.log(err)
+      res.status(500).json({
+        message: 'Internal server error.'
+      })
+      break;
+  }
+}
+
+module.exports = errorHandler
