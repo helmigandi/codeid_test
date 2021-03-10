0ca158717925cc1906bf9496d430915b87edc9ad
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-10 16:59:44 +0700

    add: create, update, and delete user

--- orchestrator/controllers/UserController.js
+++ orchestrator/controllers/UserController.js
@@ -79,6 +79,80 @@ class UserController {
       next(error.response)
     }
   }
+
+  static async addUser (req, res, next) {
+    const { access_token } = req.headers
+    try {
+      if (!access_token) next({ message: 'Forbidden' })
+      const { userName, accountNumber, emailAddress, identityNumber } = req.body
+      const { data: users } = await userService({
+        url: '/users',
+        method: 'POST',
+        data: { 
+          userName, 
+          accountNumber, 
+          emailAddress, 
+          identityNumber 
+        },
+        headers: {
+          access_token
+        }
+      })
+      await redis.del('data:users')
+      res.status(201).json(users)
+    } catch (err) {
+      next(err.response)
+    }
+  }
+
+  static async editUser (req, res, next) {
+    const { access_token } = req.headers
+    const { userId } = req.params
+    try {
+      if (!access_token) next({ message: 'Forbidden' })
+      const { userName, accountNumber, emailAddress, identityNumber } = req.body
+      const { data: users } = await userService({
+        method: 'PUT',
+        url: `/users/${userId}`,
+        data: { 
+          userName, 
+          accountNumber, 
+          emailAddress, 
+          identityNumber 
+        },
+        headers: {
+          access_token
+        }
+      })
+      if (users) {
+        await redis.del('data:users')
+        res.status(200).json(users)
+      }
+    } catch (err) {
+      next(err.response)
+    }
+  }
+
+  static async deleteUser (req, res, next) {
+    const { access_token } = req.headers
+    const { userId } = req.params
+    try {
+      if (!access_token) next({ message: 'Forbidden' })
+      const { data: users } = await userService({
+        method: 'DELETE',
+        url: `/users/${userId}`,
+        headers: {
+          access_token
+        }
+      })
+      if(users) {
+        await redis.del('data:users')
+        res.status(200).json(users)
+      }
+    } catch (err) {
+      next(err.response);
+    }
+  }
 }
 
 module.exports = UserController
--- orchestrator/routes/userRoutes.js
+++ orchestrator/routes/userRoutes.js
@@ -5,5 +5,8 @@ router.get('/', UserController.getUsers)
 router.get('/:userId', UserController.getUserById)
 router.get('/accounts/:accountNumber', UserController.getUserByAccountNumber)
 router.get('/identities/:identityNumber', UserController.getUserByIdentityNumber)
+router.post('/', UserController.addUser)
+router.put('/:userId', UserController.editUser)
+router.delete('/:userId', UserController.deleteUser)
 
 module.exports = router
