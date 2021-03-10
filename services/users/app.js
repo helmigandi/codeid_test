60d2a62a4b19958e735f6b0414af0303ffe19b37
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-10 14:18:24 +0700

    add: cors

--- services/users/app.js
+++ services/users/app.js
@@ -5,10 +5,12 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const port = process.env.PORT || 4002
 const app = express()
+const cors = require('cors')
 const { connect } = require('./config/mongodbConfig')
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(routes)
